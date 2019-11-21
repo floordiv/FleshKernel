2aeed63b72dabaadbc7a4767a07d7b9317617e72
floordiv <pavloromanenko25@gmail.com>
2019-11-21 15:28:31 +0200

    modified bootloader: shells detecting added, removed filesystem check (bad feature)

--- boot.py
+++ boot.py
@@ -47,7 +47,7 @@ def find_inf():
         result[each] = {}
         for element in content:
             result[each][element.split(':')[0]] = element.split(':')
-        print(f'[BOOT] {each}.inf: variables: {", ".join(list(result[each].values()))}')
+        print(f'[BOOT] {each}.inf: variables: {", ".join(list(result[each].keys()))}')
     return result
 
 
@@ -65,24 +65,38 @@ def valid_inf(infname, infval, required_vars=('name', 'main')):
     return True
 
 
-def fix_boot_config():
+def fix_boot_config(abort_without_shell=True):
     files = os.listdir('./system')
     result = find_inf()
+    invalid_inf = []
     if not valid_infs(result):
-        print('[BOOT-FATAL] No valid shell pointers found. Aborting...')
+        for each in result:
+            if not valid_inf(each, result[each]):
+                invalid_inf.append(each)
+    if len(invalid_inf) > 0:
+        for each in invalid_inf:
+            del result[each]
+    if len(list(result.keys())) == 0:
+        print(f'[BOOT-FATAL] Boot config recovery failed: no shells for kernel found')
+        if abort_without_shell:
+            print('[BOOT] Aborting...')
+            os.abort()
+        print('[BOOT] Aborting cancelled')
     if 'kernel.py' in files and len(result) > 0:
         print('[BOOT] Creating new boot config...')
         try:
-            files = os.listdir('./system')
-
-            default_values = [{'kern_version': 'UNKNOWN', 'kern_main': 'system/kernel.py', 'shells': [['system/shell/apollo_shell.py', {'name':
-                                                                                                                               'Default '
-                                                                                                                               'proprietary tOS shell',
-                                                                                                                           'version': 'UNKNOWN'}]],
-                               'auto_choose': False,
-                               'auto_shell_start': False, 'pre_load_exec': None}]
+            shells = []
             try:
-                with open('cfg/boot', 'w') as new_conf:
+                for elem in result:
+                    temp = [elem, result[elem]]
+                    shells.append(temp)
+            except Exception as listing_shells_exception:
+                print(f'[BOOT-ERROR] Shell listing failed: {listing_shells_exception}')
+                shells = [None]
+            default_values = [{'kern_version': 'UNKNOWN', 'kern_main': 'system/kernel.py', 'shells': shells, 'auto_choose': False,
+                               'pre_load_exec': None}]
+            try:
+                with open('./cfg/boot', 'w') as new_conf:
                     json.dump(default_values, new_conf)
             except Exception as creating_boot_config_exception:
                 print(f'[BOOT-FATAL] Creating boot config failed: {creating_boot_config_exception}')
