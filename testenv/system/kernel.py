f979a3bb99d4d340183522cd92758c3bc3dd84c6
floordiv <pavloromanenko25@gmail.com>
2019-11-27 19:32:24 +0200

    bootloader completed

--- testenv/system/kernel.py
+++ testenv/system/kernel.py
@@ -2,6 +2,6 @@ def init():
 	print('Hello, world! This is fucking kernel, bitches, it initialized)')
 	a = input('Would you like to be with me? [y/n] > ')
 	if a.lower() == 'y':
-		print('Thanks for faith to me, but buy :(')
+		print('Thanks for faith to me, but bye :(')
 	else:
 		print('Ok, ok, I understand you, but... Bye.')
